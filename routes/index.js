e5a1fa6e9ac8c2e22aba80472f73df3bbc70bb59
Momin Riyadh <mominriyadh@gmail.com>
2023-10-23 17:48:45 +0600

    fix and deploy

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express JS'});
+    let mascots = [
+        {name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+        {name: 'Tux', organization: "Linux", birth_year: 1996},
+        {name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+    ];
+    let tagline = "No programming concept is complete without a cute animal mascot.";
+    res.render('index', {
+        mascots: mascots,
+        tagline: tagline,
+        title: " Express JS"
+    });
 });
 
 module.exports = router;
