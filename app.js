6ac39d515d2d9a37c80b6e0c722c2cc6f0b1c498
momin-riyadh <mominriyadh@gmail.com>
2023-10-24 12:45:32 +0600

    fix and deploy

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const portfolioRouter = require('./routes/portfolio');
 const contactRouter = require('./routes/contact');
+const bookmarkRouter = require('./routes/bookmark');
+const fyloRouter = require('./routes/fylo');
 
 const app = express();
 
@@ -25,6 +27,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/portfolio', portfolioRouter);
 app.use('/contact', contactRouter);
+app.use('/bookmark', bookmarkRouter);
+app.use('/fylo', fyloRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
